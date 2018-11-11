813ca20968982515d7e6685adc40687f323d3bfd
Dan Hirst <danhirst123@gmail.com>
2018-11-11 22:39:14 +0000

    added getters and setters

--- WeatherStation/Year.cs
+++ WeatherStation/Year.cs
@@ -9,17 +9,42 @@ namespace WeatherStation
     class Year
     {
         //properties
+        private int year; // variable for the year
+        private string description; // Variable for the descirption for the year
+        Month [] months = new Month [1]; // sets up the monthly obseveration array
+        
+        //constructors
+        public void Year(int theYear, string theDescription, Month[]theMonths){
+            year = theYear;
+            Description = theDescription;
+            months = theMonths;
 
-        Month [] months= new Month [1]; // sets up the month array
-        private string Description; // Variable for the descirption for the year
+        }
+        public void Year(int theYear, Month[]theMonths){
+            year = theYear;
+            months = theMonths;
+        }
+
 
         // methods
         
-        // getters
-
-        //setters
         
-            
+        // getters
+        public int getYear(){
+            return year;
+        }
+        public string getDescription(){
+            return description;
+        }
 
+
+
+        // setters
+        public void setYear(int inYear){
+            year = inYear;  
+        }
+        public void setDescription(string theDescription){
+            description = theDescription;
+        }
     }
 }
