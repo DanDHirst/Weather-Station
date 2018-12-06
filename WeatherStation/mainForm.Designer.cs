a9b3238a54c8a371cb7e412c2d9fe46efbbe8882
dhirst1 <dhirst1@DM00012>
2018-12-06 15:23:31 +0000

    Chnaged the layout of the project

--- WeatherStation/mainForm.Designer.cs
+++ WeatherStation/mainForm.Designer.cs
@@ -36,6 +36,8 @@
             this.lstLocData = new System.Windows.Forms.ListBox();
             this.lstYearData = new System.Windows.Forms.ListBox();
             this.lstMonthData = new System.Windows.Forms.ListBox();
+            this.boxEditLoc = new System.Windows.Forms.GroupBox();
+            this.btnEditLoc = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // openFileDialog1
@@ -47,18 +49,18 @@
             // 
             this.lstLocations.FormattingEnabled = true;
             this.lstLocations.Location = new System.Drawing.Point(11, 25);
-            this.lstLocations.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.lstLocations.Margin = new System.Windows.Forms.Padding(2);
             this.lstLocations.Name = "lstLocations";
-            this.lstLocations.Size = new System.Drawing.Size(178, 95);
+            this.lstLocations.Size = new System.Drawing.Size(200, 147);
             this.lstLocations.TabIndex = 1;
             this.lstLocations.SelectedIndexChanged += new System.EventHandler(this.lstLocations_SelectedIndexChanged);
             // 
             // lstYears
             // 
             this.lstYears.FormattingEnabled = true;
-            this.lstYears.Location = new System.Drawing.Point(229, 25);
+            this.lstYears.Location = new System.Drawing.Point(217, 25);
             this.lstYears.Name = "lstYears";
-            this.lstYears.Size = new System.Drawing.Size(114, 95);
+            this.lstYears.Size = new System.Drawing.Size(163, 147);
             this.lstYears.TabIndex = 2;
             this.lstYears.SelectedIndexChanged += new System.EventHandler(this.lstYears_SelectedIndexChanged);
             // 
@@ -67,47 +69,67 @@
             this.lstMonths.FormattingEnabled = true;
             this.lstMonths.Location = new System.Drawing.Point(386, 25);
             this.lstMonths.Name = "lstMonths";
-            this.lstMonths.Size = new System.Drawing.Size(109, 95);
+            this.lstMonths.Size = new System.Drawing.Size(192, 147);
             this.lstMonths.TabIndex = 3;
             this.lstMonths.SelectedIndexChanged += new System.EventHandler(this.lstMonths_SelectedIndexChanged);
             // 
             // lstLocData
             // 
             this.lstLocData.FormattingEnabled = true;
-            this.lstLocData.Location = new System.Drawing.Point(12, 125);
+            this.lstLocData.Location = new System.Drawing.Point(11, 181);
             this.lstLocData.Name = "lstLocData";
-            this.lstLocData.Size = new System.Drawing.Size(177, 108);
+            this.lstLocData.Size = new System.Drawing.Size(200, 173);
             this.lstLocData.TabIndex = 4;
             // 
             // lstYearData
             // 
             this.lstYearData.FormattingEnabled = true;
-            this.lstYearData.Location = new System.Drawing.Point(229, 138);
+            this.lstYearData.Location = new System.Drawing.Point(217, 181);
             this.lstYearData.Name = "lstYearData";
-            this.lstYearData.Size = new System.Drawing.Size(114, 95);
+            this.lstYearData.Size = new System.Drawing.Size(163, 173);
             this.lstYearData.TabIndex = 5;
             // 
             // lstMonthData
             // 
             this.lstMonthData.FormattingEnabled = true;
-            this.lstMonthData.Location = new System.Drawing.Point(386, 138);
+            this.lstMonthData.Location = new System.Drawing.Point(386, 181);
             this.lstMonthData.Name = "lstMonthData";
-            this.lstMonthData.Size = new System.Drawing.Size(109, 95);
+            this.lstMonthData.Size = new System.Drawing.Size(192, 173);
             this.lstMonthData.TabIndex = 6;
             // 
-            // Form1
+            // boxEditLoc
+            // 
+            this.boxEditLoc.Location = new System.Drawing.Point(784, 25);
+            this.boxEditLoc.Name = "boxEditLoc";
+            this.boxEditLoc.Size = new System.Drawing.Size(406, 182);
+            this.boxEditLoc.TabIndex = 7;
+            this.boxEditLoc.TabStop = false;
+            this.boxEditLoc.Text = "Edit Location";
+            // 
+            // btnEditLoc
+            // 
+            this.btnEditLoc.Location = new System.Drawing.Point(11, 500);
+            this.btnEditLoc.Name = "btnEditLoc";
+            this.btnEditLoc.Size = new System.Drawing.Size(178, 51);
+            this.btnEditLoc.TabIndex = 8;
+            this.btnEditLoc.Text = "Edit location";
+            this.btnEditLoc.UseVisualStyleBackColor = true;
+            // 
+            // mainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1271, 624);
+            this.Controls.Add(this.btnEditLoc);
+            this.Controls.Add(this.boxEditLoc);
             this.Controls.Add(this.lstMonthData);
             this.Controls.Add(this.lstYearData);
             this.Controls.Add(this.lstLocData);
             this.Controls.Add(this.lstMonths);
             this.Controls.Add(this.lstYears);
             this.Controls.Add(this.lstLocations);
-            this.Name = "Form1";
-            this.Text = "Form1";
+            this.Name = "mainForm";
+            this.Text = "Weather Station application";
             this.ResumeLayout(false);
 
         }
@@ -122,6 +144,8 @@
         private System.Windows.Forms.ListBox lstLocData;
         private System.Windows.Forms.ListBox lstYearData;
         private System.Windows.Forms.ListBox lstMonthData;
+        private System.Windows.Forms.GroupBox boxEditLoc;
+        private System.Windows.Forms.Button btnEditLoc;
     }
 }
 
